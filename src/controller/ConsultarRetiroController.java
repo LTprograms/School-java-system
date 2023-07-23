e56bd1c2fc716e717cd09c9f9fa0475d61b4f899
Luis LT <105554066+LTprograms@users.noreply.github.com>
2023-07-23 13:23:00 -0500

    consultarRetiro

---
+++ src/controller/ConsultarRetiroController.java
@@ -0,0 +1,97 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.table.DefaultTableModel;
+
+import model.Alumno;
+import model.Curso;
+import model.Matricula;
+import model.Retiro;
+import view.ConsultarMatriculas;
+
+public class ConsultarRetiroController {
+	ConsultarMatriculas view;
+	ArrayList<Alumno> listaAlumnos;
+	ArrayList<Curso> listaCursos;
+	ArrayList<Matricula> listaMatriculas;
+	ArrayList<Retiro> listaRetiros;
+	public ConsultarRetiroController(ArrayList<Alumno> alumnos, ArrayList<Curso> cursos, ArrayList<Matricula> matriculas, ArrayList<Retiro> retiros) {
+		view = new ConsultarMatriculas();
+		this.listaAlumnos = alumnos;
+		this.listaCursos= cursos;
+		this.listaMatriculas = matriculas;
+		this.listaRetiros = retiros;
+		fillTable();
+		this.view.addAlumno.addActionListener( new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				AddAlumnoController fr = new AddAlumnoController(listaAlumnos, listaCursos, listaMatriculas, listaRetiros);
+				view.dispose();
+				fr.run();
+			}
+            
+        });
+		this.view.addCurso.addActionListener( new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				AddCursoController fr = new AddCursoController(listaAlumnos, listaCursos, listaMatriculas, listaRetiros);
+				view.dispose();
+				fr.run();
+			}
+            
+        });
+		this.view.updateAlumno.addActionListener( new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				ConsultarAlumnosController fr = new ConsultarAlumnosController(listaAlumnos, listaCursos, listaMatriculas, listaRetiros);
+				view.dispose();
+				fr.run();
+			}
+            
+        });
+		this.view.updateCurso.addActionListener( new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				ConsultarCursoController fr = new ConsultarCursoController(listaAlumnos, listaCursos, listaMatriculas, listaRetiros);
+				view.dispose();
+				fr.run();
+			}           
+        });
+		this.view.addMatricula.addActionListener( new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				AddMatriculaController fr = new AddMatriculaController(listaAlumnos, listaCursos, listaMatriculas, listaRetiros);
+				view.dispose();
+				fr.run();
+			}           
+        });
+	}
+	public void run() {
+		this.view.setLocationRelativeTo(null);
+        this.view.setVisible(true);
+	}
+	private void  fillTable() {
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("Codigo");
+        model.addColumn("Matricula");
+        model.addColumn("Fecha");
+        model.addColumn("Hora");
+        for (Retiro r : this.listaRetiros) {
+        	model.addRow(new Object[] {r.getNumRetiro(), r.getNumMatricula(), r.getFecha(), r.getHora()});
+        }
+        this.view.table.setModel(model);
+	}
+}
---
+++ src/view/ConsultarRetiro.java
@@ -0,0 +1,136 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class ConsultarRetiro extends JFrame {
+
+private JPanel contentPane;
+	
+	public JMenuItem addAlumno;
+	public JMenuItem addCurso;
+	public JMenuItem updateAlumno;
+	public JMenuItem updateCurso;
+	
+	public JMenuItem addMatricula;
+	public JMenuItem addRetiro;
+	public JMenuItem updateMatricula;
+	public JMenuItem updateRetiro;
+	private JScrollPane scrollPane;
+	public JTable table;
+	private JLabel lblNewLabel;
+	public JTextField txtCodigo;
+	public JButton btnModificar;
+	public JButton btnEliminar;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ConsultarRetiro frame = new ConsultarRetiro();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ConsultarRetiro() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 434, 22);
+		contentPane.add(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Mantenimiento");
+		menuBar.add(mnNewMenu);
+		
+		JMenu mnNewMenu_1 = new JMenu("Adicionar");
+		mnNewMenu.add(mnNewMenu_1);
+		
+		addAlumno = new JMenuItem("Alumno");
+		mnNewMenu_1.add(addAlumno);
+		
+		addCurso = new JMenuItem("Curso");
+		mnNewMenu_1.add(addCurso);
+		
+		JMenu mnNewMenu_2 = new JMenu("Consultar");
+		mnNewMenu.add(mnNewMenu_2);
+		
+		updateAlumno = new JMenuItem("Alumno");
+		mnNewMenu_2.add(updateAlumno);
+		
+		updateCurso = new JMenuItem("Curso");
+		mnNewMenu_2.add(updateCurso);
+		
+		JMenu mnRegistro = new JMenu("Registro");
+		menuBar.add(mnRegistro);
+		
+		JMenu mnNewMenu_3 = new JMenu("Adicionar");
+		mnRegistro.add(mnNewMenu_3);
+		
+		addMatricula = new JMenuItem("Matricula");
+		mnNewMenu_3.add(addMatricula);
+		
+		addRetiro = new JMenuItem("Retiro");
+		mnNewMenu_3.add(addRetiro);
+		
+		JMenu mnNewMenu_3_1 = new JMenu("Consultar");
+		mnRegistro.add(mnNewMenu_3_1);
+		
+		updateMatricula = new JMenuItem("Matricula");
+		mnNewMenu_3_1.add(updateMatricula);
+		
+		updateRetiro = new JMenuItem("Retiro");
+		mnNewMenu_3_1.add(updateRetiro);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 79, 414, 171);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+		lblNewLabel = new JLabel("Codigo:");
+		lblNewLabel.setBounds(10, 52, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		txtCodigo = new JTextField();
+		txtCodigo.setBounds(66, 49, 114, 20);
+		contentPane.add(txtCodigo);
+		txtCodigo.setColumns(10);
+		
+		btnModificar = new JButton("Modificar");
+		btnModificar.setBounds(203, 48, 89, 23);
+		contentPane.add(btnModificar);
+		
+		btnEliminar = new JButton("Eliminar");
+		btnEliminar.setBounds(319, 48, 89, 23);
+		contentPane.add(btnEliminar);
+	}
+
+}
